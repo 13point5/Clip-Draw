f1d23e094d8725ac50d0051bead078c90db1fa6b
Bharath Sriraam R R <albharath.1305@gmail.com>
2020-08-28 13:40:08 +0530

    it looks nice

--- clipDraw.js
+++ clipDraw.js
@@ -1,20 +1,20 @@
 import { getCoords, isSameVertex } from "./coords.js";
 import { getRandomColor, getInvertedColor } from "./color.js";
 import {
-    clearCanvas,
-    drawSingleLine,
-    drawSingleCircle,
-    drawAnchors,
-    drawEdges,
-    drawPolygon,
-    completePolygon,
+  clearCanvas,
+  drawSingleLine,
+  drawSingleCircle,
+  drawAnchors,
+  drawEdges,
+  drawPolygon,
+  completePolygon,
 } from "./draw.js";
 
 const canvas = document.getElementById("canvas");
 
 if (!canvas.getContext) {
-    alert("Canvas is not supported in this browser ☹");
-    window.stop();
+  alert("Canvas is not supported in this browser ☹");
+  window.stop();
 }
 
 const ctx = canvas.getContext("2d");
@@ -30,6 +30,9 @@ const clearCanvasBtn = document.getElementById("clear-canvas");
 const scrollTopBtn = document.querySelector(".scrollTopBtn");
 
 // Settings
+const settingsBtn = document.getElementById("settings-btn");
+const settingsBox = document.getElementById("settings-box");
+
 const cWidthInput = document.getElementById("cWidth");
 const cHeightInput = document.getElementById("cHeight");
 
@@ -45,9 +48,9 @@ const clipPathCodeElement = document.getElementById("clip-path-code");
 const changeCanvasDimsForm = document.getElementById("canvas-dims-form");
 
 const actionModes = Object.freeze({
-    draw: 0,
-    reshape: 1,
-    remove: 2,
+  draw: 0,
+  reshape: 1,
+  remove: 2,
 });
 
 let vertices = [];
@@ -63,206 +66,205 @@ canvas.style.cursor = "crosshair";
 ctx.lineWidth = strokeWidth;
 
 const resetVars = () => {
-    vertices = [];
-    draggable = false;
-    isMouseDown = false;
+  vertices = [];
+  draggable = false;
+  isMouseDown = false;
 
-    cWidth = canvas.width;
-    cHeight = canvas.height;
-    currActionMode = actionModes.draw;
-    setClipPathCode();
+  cWidth = canvas.width;
+  cHeight = canvas.height;
+  currActionMode = actionModes.draw;
+  setClipPathCode();
 };
 
 const getClipPathPoints = () => {
-    const numOfVertices = vertices.length;
+  const numOfVertices = vertices.length;
 
-    if (numOfVertices === 0) {
-        return [];
-    }
+  if (numOfVertices === 0) {
+    return [];
+  }
 
-    let clipVertices = vertices;
+  let clipVertices = vertices;
 
-    if (isSameVertex(vertices[0], vertices[numOfVertices - 1])) {
-        clipVertices = vertices.slice(0, numOfVertices - 1);
-    }
+  if (isSameVertex(vertices[0], vertices[numOfVertices - 1])) {
+    clipVertices = vertices.slice(0, numOfVertices - 1);
+  }
 
-    const clipPathPoints = clipVertices.map((vertex) => {
-        const xPercent = ((vertex.x / cWidth) * 100).toFixed(0);
-        const yPercent = ((vertex.y / cHeight) * 100).toFixed(0);
-        return `${xPercent}% ${yPercent}%`;
-    });
+  const clipPathPoints = clipVertices.map((vertex) => {
+    const xPercent = ((vertex.x / cWidth) * 100).toFixed(0);
+    const yPercent = ((vertex.y / cHeight) * 100).toFixed(0);
+    return `${xPercent}% ${yPercent}%`;
+  });
 
-    return clipPathPoints;
+  return clipPathPoints;
 };
 
 const setClipPathCode = () => {
-    const clipPathPoints = getClipPathPoints();
+  const clipPathPoints = getClipPathPoints();
 
-    if (clipPathPoints.length === 0) {
-        clipPathCodeElement.innerHTML = "";
-        return;
-    }
+  if (clipPathPoints.length === 0) {
+    clipPathCodeElement.innerHTML = "";
+    return;
+  }
 
-    clipPathCodeElement.innerHTML = "clip-path: polygon(";
-    clipPathPoints.forEach((point, idx) => {
-        const pointBgColor = vertices[idx].color;
-        const pointTextColor = getInvertedColor(pointBgColor);
-        clipPathCodeElement.innerHTML += `<span style="color:${pointTextColor};background-color:${pointBgColor};">${point}</span>`;
-        clipPathCodeElement.innerHTML +=
-            idx !== clipPathPoints.length - 1 ? ", " : "";
-    });
+  clipPathCodeElement.innerHTML = "clip-path: polygon(";
+  clipPathPoints.forEach((point, idx) => {
+    const pointBgColor = vertices[idx].color;
+    const pointTextColor = getInvertedColor(pointBgColor);
+    clipPathCodeElement.innerHTML += `<span style="color:${pointTextColor};background-color:${pointBgColor};">${point}</span>`;
+    clipPathCodeElement.innerHTML +=
+      idx !== clipPathPoints.length - 1 ? ", " : "";
+  });
 
-    clipPathCodeElement.innerHTML += ");";
+  clipPathCodeElement.innerHTML += ");";
 };
 
 const handleStart = (cursorMode, e) => {
-    const coords = getCoords(cursorMode, e);
+  const coords = getCoords(cursorMode, e);
 
-    isMouseDown = true;
+  isMouseDown = true;
 
-    if (currActionMode !== actionModes.draw) {
-        let removeIdx = -1;
+  if (currActionMode !== actionModes.draw) {
+    let removeIdx = -1;
 
-        for (let idx = 0; idx < vertices.length; idx++) {
-            drawSingleCircle(ctx, vertices[idx], anchorRadius);
-            if (ctx.isPointInPath(coords.x, coords.y)) {
-                if (currActionMode === actionModes.reshape) {
-                    draggable = idx + 1;
-                    break;
-                } else if (currActionMode === actionModes.remove) {
-                    removeIdx = idx;
-                    break;
-                }
-            }
+    for (let idx = 0; idx < vertices.length; idx++) {
+      drawSingleCircle(ctx, vertices[idx], anchorRadius);
+      if (ctx.isPointInPath(coords.x, coords.y)) {
+        if (currActionMode === actionModes.reshape) {
+          draggable = idx + 1;
+          break;
+        } else if (currActionMode === actionModes.remove) {
+          removeIdx = idx;
+          break;
         }
-
-        if (removeIdx != -1) {
-            const numOfVertices = vertices.length;
-
-            if (removeIdx == 0) {
-                if (numOfVertices == 2) {
-                    vertices = vertices.slice(1);
-                } else if (numOfVertices == 1) {
-                    vertices = [];
-                } else {
-                    vertices = vertices.slice(1, numOfVertices - 1);
-                }
-            } else {
-                vertices.splice(removeIdx, 1);
-            }
-
-            if (vertices.length >= 3) {
-                completePolygon(ctx, vertices);
-            } else {
-                drawEdges(ctx, vertices);
-            }
-
-            drawAnchors(ctx, vertices, anchorRadius);
-        }
-    } else {
-        if (vertices.length === 0) {
-            vertices.push({
-                x: coords.x,
-                y: coords.y,
-                color: getRandomColor(),
-            });
-        }
+      }
     }
 
-    setClipPathCode();
-};
+    if (removeIdx != -1) {
+      const numOfVertices = vertices.length;
 
-const handleMove = (cursorMode, e) => {
-    if (!isMouseDown) {
-        return;
-    }
-
-    const coords = getCoords(cursorMode, e);
-
-    if (currActionMode !== actionModes.draw) {
-        if (draggable) {
-            vertices[draggable - 1] = {
-                color: vertices[draggable - 1].color,
-                ...coords,
-            };
-            // vertices[draggable - 1].x = coords.x;
-            // vertices[draggable - 1].y = coords.y;
-            if (draggable === 1) {
-                vertices[vertices.length - 1] = {
-                    color: vertices[vertices.length - 1].color,
-                    ...coords,
-                };
-            }
-            drawPolygon(ctx, vertices, anchorRadius);
+      if (removeIdx == 0) {
+        if (numOfVertices == 2) {
+          vertices = vertices.slice(1);
+        } else if (numOfVertices == 1) {
+          vertices = [];
+        } else {
+          vertices = vertices.slice(1, numOfVertices - 1);
         }
-    } else {
+      } else {
+        vertices.splice(removeIdx, 1);
+      }
+
+      if (vertices.length >= 3) {
+        completePolygon(ctx, vertices);
+      } else {
         drawEdges(ctx, vertices);
+      }
 
-        const lastPoint = vertices[vertices.length - 1];
-        drawSingleLine(ctx, lastPoint, coords);
+      drawAnchors(ctx, vertices, anchorRadius);
     }
-
-    setClipPathCode();
-};
-
-const handleEnd = (cursorMode, e) => {
-    isMouseDown = false;
-
-    if (currActionMode !== actionModes.draw) {
-        draggable = false;
-        return;
-    }
-
-    const coords = getCoords(cursorMode, e, true);
-
-    vertices.push({
+  } else {
+    if (vertices.length === 0) {
+      vertices.push({
         x: coords.x,
         y: coords.y,
         color: getRandomColor(),
-    });
+      });
+    }
+  }
 
-    ctx.lineWidth = strokeWidth;
+  setClipPathCode();
+};
 
+const handleMove = (cursorMode, e) => {
+  if (!isMouseDown) {
+    return;
+  }
+
+  const coords = getCoords(cursorMode, e);
+
+  if (currActionMode !== actionModes.draw) {
+    if (draggable) {
+      vertices[draggable - 1] = {
+        color: vertices[draggable - 1].color,
+        ...coords,
+      };
+      // vertices[draggable - 1].x = coords.x;
+      // vertices[draggable - 1].y = coords.y;
+      if (draggable === 1) {
+        vertices[vertices.length - 1] = {
+          color: vertices[vertices.length - 1].color,
+          ...coords,
+        };
+      }
+      drawPolygon(ctx, vertices, anchorRadius);
+    }
+  } else {
     drawEdges(ctx, vertices);
+
+    const lastPoint = vertices[vertices.length - 1];
+    drawSingleLine(ctx, lastPoint, coords);
+  }
+
+  setClipPathCode();
+};
+
+const handleEnd = (cursorMode, e) => {
+  isMouseDown = false;
+
+  if (currActionMode !== actionModes.draw) {
+    draggable = false;
+    return;
+  }
+
+  const coords = getCoords(cursorMode, e, true);
+
+  vertices.push({
+    x: coords.x,
+    y: coords.y,
+    color: getRandomColor(),
+  });
+
+  ctx.lineWidth = strokeWidth;
+
+  drawEdges(ctx, vertices);
 };
 
 const handleMouseDown = (e) => {
-    handleStart("mouse", e);
+  handleStart("mouse", e);
 };
 
 const handleMouseMove = (e) => {
-    handleMove("mouse", e);
+  handleMove("mouse", e);
 };
 
 const handleMouseUp = (e) => {
-    handleEnd("mouse", e);
+  handleEnd("mouse", e);
 };
 
 const handleTouchStart = (e) => {
-    handleStart("touch", e);
-    e.preventDefault();
+  handleStart("touch", e);
+  e.preventDefault();
 };
 
 const handleTouchMove = (e) => {
-    handleMove("touch", e);
-    e.preventDefault();
+  handleMove("touch", e);
+  e.preventDefault();
 };
 
 const handleTouchEnd = (e) => {
-    handleEnd("touch", e);
+  handleEnd("touch", e);
 };
 
 const handleScroll = () => {
-    // do something on scroll
-    const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-    console.log(rootElement.scrollTop / scrollTotal > 0.8);
-    if (rootElement.scrollTop / scrollTotal > 0.8) {
-        //show button
-        scrollTopBtn.classList.add("showBtn");
-    } else {
-        //hide button
-        scrollTopBtn.classList.remove("showBtn");
-    }
+  // do something on scroll
+  const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.8) {
+    //show button
+    scrollTopBtn.classList.add("showBtn");
+  } else {
+    //hide button
+    scrollTopBtn.classList.remove("showBtn");
+  }
 };
 
 document.addEventListener("scroll", handleScroll);
@@ -276,95 +278,99 @@ canvas.addEventListener("touchmove", handleTouchMove, false);
 canvas.addEventListener("touchend", handleTouchEnd, false);
 
 scrollTopBtn.onclick = () => {
-    //scroll to top logic
-    rootElement.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
+  //scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+settingsBtn.onclick = () => {
+  settingsBox.style.opacity = 1 - settingsBox.style.opacity;
 };
 
 clearCanvasBtn.onclick = () => {
-    canvas.style.cursor = "crosshair";
-    resetVars();
-    clearCanvas(ctx);
+  canvas.style.cursor = "crosshair";
+  resetVars();
+  clearCanvas(ctx);
 };
 
 reshapePolygonBtn.onclick = () => {
-    canvas.style.cursor = "move";
-    currActionMode = actionModes.reshape;
+  canvas.style.cursor = "move";
+  currActionMode = actionModes.reshape;
 
-    setClipPathCode();
+  setClipPathCode();
 
-    completePolygon(ctx, vertices);
-    drawAnchors(ctx, vertices, anchorRadius);
+  completePolygon(ctx, vertices);
+  drawAnchors(ctx, vertices, anchorRadius);
 };
 
 addVertexBtn.onclick = () => {
-    if (currActionMode === actionModes.draw) return;
+  if (currActionMode === actionModes.draw) return;
 
-    canvas.style.cursor = "crosshair";
-    currActionMode = actionModes.draw;
+  canvas.style.cursor = "crosshair";
+  currActionMode = actionModes.draw;
 
-    setClipPathCode();
+  setClipPathCode();
 
-    vertices = vertices.slice(0, vertices.length - 1);
-    drawEdges(ctx, vertices);
+  vertices = vertices.slice(0, vertices.length - 1);
+  drawEdges(ctx, vertices);
 };
 
 removeVertexBtn.onclick = () => {
-    canvas.style.cursor = "pointer";
-    currActionMode = actionModes.remove;
+  canvas.style.cursor = "pointer";
+  currActionMode = actionModes.remove;
 
-    setClipPathCode();
+  setClipPathCode();
 
-    completePolygon(ctx, vertices);
-    drawAnchors(ctx, vertices, anchorRadius);
+  completePolygon(ctx, vertices);
+  drawAnchors(ctx, vertices, anchorRadius);
 };
 
 changeCanvasDimsForm.onsubmit = (e) => {
-    e.preventDefault();
-    const newWidth = cWidthInput.value || cWidth;
-    const newHeight = cHeightInput.value || cHeight;
+  e.preventDefault();
+  const newWidth = cWidthInput.value || cWidth;
+  const newHeight = cHeightInput.value || cHeight;
 
-    if (newWidth <= 0 || newHeight <= 0) {
-        alert("Enter valid dimensions for the canvas");
-        return;
-    }
+  if (newWidth <= 0 || newHeight <= 0) {
+    alert("Enter valid dimensions for the canvas");
+    return;
+  }
 
-    vertices = vertices.map((vertex) => {
-        return {
-            x: Math.round((vertex.x / cWidth) * newWidth),
-            y: Math.round((vertex.y / cHeight) * newHeight),
-            color: vertex.color,
-        };
-    });
+  vertices = vertices.map((vertex) => {
+    return {
+      x: Math.round((vertex.x / cWidth) * newWidth),
+      y: Math.round((vertex.y / cHeight) * newHeight),
+      color: vertex.color,
+    };
+  });
 
-    canvas.width = newWidth;
-    canvas.height = newHeight;
+  canvas.width = newWidth;
+  canvas.height = newHeight;
 
-    cWidth = newWidth;
-    cHeight = newHeight;
+  cWidth = newWidth;
+  cHeight = newHeight;
 
-    ctx.lineWidth = strokeWidth;
+  ctx.lineWidth = strokeWidth;
 
-    drawEdges(ctx, vertices);
+  drawEdges(ctx, vertices);
 };
 
 strokeIncBtn.onclick = () => {
-    ctx.lineWidth = ++strokeWidth;
-    strokeWidthTxt.textContent = ctx.lineWidth;
+  ctx.lineWidth = ++strokeWidth;
+  strokeWidthTxt.textContent = ctx.lineWidth;
 };
 
 strokeDecBtn.onclick = () => {
-    ctx.lineWidth = Math.max(2, --strokeWidth);
-    strokeWidthTxt.textContent = ctx.lineWidth;
+  ctx.lineWidth = Math.max(2, --strokeWidth);
+  strokeWidthTxt.textContent = ctx.lineWidth;
 };
 
 anchorIncBtn.onclick = () => {
-    anchorWidthTxt.textContent = ++anchorRadius;
+  anchorWidthTxt.textContent = ++anchorRadius;
 };
 
 anchorDecBtn.onclick = () => {
-    anchorRadius = Math.max(5, --anchorRadius);
-    anchorWidthTxt.textContent = anchorRadius;
+  anchorRadius = Math.max(5, --anchorRadius);
+  anchorWidthTxt.textContent = anchorRadius;
 };
--- coords.js
+++ coords.js
@@ -1,46 +1,45 @@
 const getMouseCoords = (e) => {
-    if (e.offsetX) {
-        return {
-            x: e.offsetX,
-            y: e.offsetY,
-        };
-    } else if (e.layerX) {
-        return {
-            x: e.layerX,
-            y: e.layerY,
-        };
-    }
+  if (e.offsetX) {
+    return {
+      x: e.offsetX,
+      y: e.offsetY,
+    };
+  } else if (e.layerX) {
+    return {
+      x: e.layerX,
+      y: e.layerY,
+    };
+  }
 };
 
 const getTouchCoords = (e, end) => {
-    const touch = end ? e.changedTouches[0] : e.touches[0];
-    return {
-        x: Math.min(
-            touch.target.width,
-            Math.max(0, touch.pageX - touch.target.offsetLeft)
-        ),
-        y: Math.min(
-            touch.target.height,
-            Math.max(0, touch.pageY - touch.target.offsetTop)
-        ),
-    };
+  const touch = end ? e.changedTouches[0] : e.touches[0];
+  return {
+    x: Math.min(
+      touch.target.width,
+      Math.max(0, touch.pageX - touch.target.offsetLeft)
+    ),
+    y: Math.min(
+      touch.target.height,
+      Math.max(0, touch.pageY - touch.target.offsetTop)
+    ),
+  };
 };
 
 const getCoords = (mode, e, end = false) => {
-    if (mode === "touch") {
-        console.log(mode, e);
-        return getTouchCoords(e, end);
-    } else if (mode === "mouse") {
-        return getMouseCoords(e);
-    } else {
-        alert("An anomaly has occured!");
-        window.stop();
-    }
+  if (mode === "touch") {
+    return getTouchCoords(e, end);
+  } else if (mode === "mouse") {
+    return getMouseCoords(e);
+  } else {
+    alert("An anomaly has occured!");
+    window.stop();
+  }
 };
 
 const isSameVertex = (v1, v2) => {
-    const keys = ["x", "y", "color"];
-    return keys.every((key) => v1[key] === v2[key]);
+  const keys = ["x", "y", "color"];
+  return keys.every((key) => v1[key] === v2[key]);
 };
 
 export { getCoords, isSameVertex };
